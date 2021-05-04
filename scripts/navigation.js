3fc855e3d82213779c42552185733111b0968c3e
Slakkan <lnpastinante@gmail.com>
2021-05-04 21:35:56 +0200

    initial commit

---
+++ scripts/navigation.js
@@ -0,0 +1,42 @@
+
+const sections = Array.from(document.querySelectorAll('section'));
+const links = Array.from(document.querySelectorAll('nav > ul > li'));
+
+let currentSection = sections[0];
+
+const mobileOptions = {
+  rootMargin: "0px 0px -1px 0px",
+  threshold: 0.5
+};
+
+const desktopOptions = {
+  rootMargin: "-100px 0px 0px 0px",
+  threshold: 0.5
+};
+
+const desktopObserver = new IntersectionObserver((entries) => {
+  if (window.innerWidth >= 768) {
+    observeFunction(entries);
+  }
+}, desktopOptions);
+
+const mobileObserver = new IntersectionObserver((entries) => {
+  if (window.innerWidth < 768) {
+    observeFunction(entries);
+  }
+}, mobileOptions);
+
+const observeFunction = (entries) => {
+  entries.forEach(entry => {
+    const index = sections.findIndex(section => section === entry.target);
+    if (entry.isIntersecting) {
+      links[index].classList.add('link-active');
+      currentSection = entry.target;
+    } else {
+      links[index].classList.remove('link-active');
+    }
+  });
+};
+
+sections.forEach(section => mobileObserver.observe(section));
+sections.forEach(section => desktopObserver.observe(section));
