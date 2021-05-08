191326f9715a9449786e8551a6d46b24be29c538
Slakkan <lnpastinante@gmail.com>
2021-05-08 15:41:39 +0200

    added carousel component, optimized image size, fixed some style issues, added lit-html lib

---
+++ components/carousel.js
@@ -0,0 +1,56 @@
+import { html, render } from '../node_modules/lit-html/lit-html.js';
+
+const carousels = Array.from(document.querySelectorAll(".carousel"));
+const slides = [];
+
+carousels.forEach(carousel => {
+  const carouselSlides = Array.from(carousel.querySelectorAll(".carousel > .carousel__slide"));
+  slides.push(carouselSlides);
+});
+
+
+class Carousel {
+  currentSlide = 0;
+  
+  constructor(slides, carousel) {
+    this.slides = slides;
+    this.carousel = carousel
+  }
+
+  previous(e) {
+    if (e.type === "click" || e.type === "keyup" && e.key === "Enter") {
+      this.currentSlide--;
+      this.slides.forEach(slide => slide.style.transform = `translateX(${ -100 * this.currentSlide }%)`);
+      render(this.getTemplate(), this.carousel)
+    }
+  }
+
+  next(e) {
+    if (e.type === "click" || e.type === "keyup" && e.key === "Enter") {
+      this.currentSlide++;
+      this.slides.forEach(slide => slide.style.transform = `translateX(${ -100 * this.currentSlide }%)`);
+      render(this.getTemplate(), this.carousel)
+    }
+  }
+
+  getTemplate() {
+    return html`
+    <button ?disabled=${this.currentSlide === 0} @click=${ this.previous.bind(this) } @keyup=${ this.previous.bind(this) }"
+      class="carousel__arrow icon">
+      <svg tabindex="1" viewBox="0 0 24 24">
+        <use class="carousel__arrow-icon" href="./icons/arrow-left.svg#icon"></use>
+      </svg>
+    </button>
+    ${ slides.map(slide => html`${ slide }`) }
+    <button ?disabled=${this.currentSlide === this.slides.length - 1} @click=${ this.next.bind(this) } @keyup=${ this.next.bind(this) } class="carousel__arrow carousel__arrow--right icon">
+      <svg tabindex="2" viewBox="0 0 24 24">
+        <use class="carousel__arrow-icon" href="./icons/arrow-right.svg#icon"></use>
+      </svg>
+    `;
+  }
+}
+
+carousels.forEach((carousel, index) => {
+  const carouselClass = new Carousel(slides[index], carousel);
+  render(carouselClass.getTemplate(), carousel);
+});
